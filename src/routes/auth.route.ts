e7fdcfb72281b2c3e35e7c7aa21f46df09780f75
Duong Duc Anh <duongdoican@gmail.com>
2023-02-10 02:56:49 +0700

    update

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { register, login, refresh } from "../controllers/auth.controller.ts";
+import * as rate from "../middleware/rateLimit.middleware.ts";
 
 let router = express.Router();
 
 let authRoute = (app: any) => {
-  router.post("/register", register);
-  router.post("/login", login);
-  router.post("/refresh", refresh)
-  return app.use("/auth", router);
+  router.post("/register", rate.auth, register);
+  router.post("/login", rate.auth, login);
+  router.post("/refresh", rate.auth, refresh);
+  return app.use("/auth", rate.auth, router);
 };
 
 export default authRoute;
--- src/routes/cat.route.ts
+++ src/routes/cat.route.ts
@@ -8,16 +8,17 @@ import {
   deleteMany,
   update,
 } from "../controllers/cat.controller.ts";
-import * as auth from "../middleware/authorization.middleware.ts"
+import * as auth from "../middleware/authorization.middleware.ts";
+import * as rate from "../middleware/rateLimit.middleware.ts";
 
 let router = express.Router();
 
 let catRoute = (app: any) => {
-  router.get("/", auth.authorizations, findAll);
-  router.get("/:id",auth.authorizations, findById);
-  router.post("/", auth.authorizations, create);
-  router.delete("/:id",auth.authorizations, deleteById);
-  router.put("/:id",auth.authorizations, update);
+  router.get("/", rate.cat, auth.authorizations, findAll);
+  router.get("/:id", rate.cat, auth.authorizations, findById);
+  router.post("/", rate.cat, auth.authorizations, create);
+  router.delete("/:id", rate.cat, auth.authorizations, deleteById);
+  router.put("/:id", rate.cat, auth.authorizations, update);
   return app.use("/cat", router);
 };
 
